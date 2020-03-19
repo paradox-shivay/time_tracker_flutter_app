3e1d19158524205114835673128b4882b0c8d6b2
Shivay <shivay.paradox@gmail.com>
2020-03-19 21:21:59 +0530

    2nd

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'app/sign_in/sign_in_page.dart';
+
 void main(){
-  runApp(MyaApp());
+  runApp(MyApp());
 }
-class MyaApp extends StatelessWidget {
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: SignInPage(),
+    );
   }
 }

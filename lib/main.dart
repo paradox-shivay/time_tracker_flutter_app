f21c5585e85b95aea7f983b5ae81bac45d54327d
Shivay <shivay.paradox@gmail.com>
2020-03-19 18:50:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyaApp());
+}
+class MyaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
